935f72d44646daa1cf6fd9d7a209f0270026e466
jyang5 <jyang5@academic.rrc.ca>
2024-04-12 12:36:35 -0500

    tags admin page

--- full_stack_web_ecommerce_site_Glamorize_Me/app/admin/tags.rb
+++ full_stack_web_ecommerce_site_Glamorize_Me/app/admin/tags.rb
@@ -14,5 +14,26 @@ ActiveAdmin.register Tag do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+
+  permit_params :tag_name
+
+  index do
+    selectable_column
+    id_column
+    column :tag_name
+    actions
+  end
+
+  filter :tag_name
+  filter :created_at
+  #filter :products_name_cont, as: 'string'  # This allows filtering categories based on the names of their products
+
+  form do |f|
+    f.inputs do
+      f.input :tag_name
+    end
+    f.actions
+  end
+
+
 end
--- full_stack_web_ecommerce_site_Glamorize_Me/app/models/tag.rb
+++ full_stack_web_ecommerce_site_Glamorize_Me/app/models/tag.rb
@@ -3,4 +3,14 @@ class Tag < ApplicationRecord
   has_many :products, through: :product_tags
 
   validates :tag_name, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    super - ['id', 'created_at', 'updated_at']
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    # 'products' is included to allow searches that traverse through the Category -> Products relationship.
+    ["product_tag"]
+  end
+
 end
